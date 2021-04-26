a643adf8cdf88813380d5c0a430e36fa551e1e9f
Sameer Jagtap <jagtap.sameer00@gmail.com>
2021-04-26 21:22:23 +0530

    Added view and Data driven architecture

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  Concentration
-//
-//  Created by postgres on 4/26/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+           flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Not found")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
